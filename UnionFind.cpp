70db33be89b5afd11718660aa3e0c6a4048a66a8
DevLaukey <devlaukeymwaura@gmail.com>
2023-12-06 09:26:37 +0300

    Added the task2 union

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -2,74 +2,61 @@
 #include <unordered_map>
 #include <vector>
 
-class UnionFind
-{
+class UnionFind {
 private:
     std::unordered_map<int, int> parent;
 
 public:
     // Function to add a set with a single element
-    void makeSet(int symbol)
-    {
-        if (parent.find(symbol) == parent.end())
-        {
+    void makeSet(int symbol) {
+        if (parent.find(symbol) == parent.end()) {
             parent[symbol] = symbol;
         }
     }
 
     // Function to find the representative of a set (with path compression)
-    int find(int symbol)
-    {
-        if (parent[symbol] != symbol)
-        {
-            parent[symbol] = find(parent[symbol]); // Path compression
+    int find(int symbol) {
+        if (parent[symbol] != symbol) {
+            parent[symbol] = find(parent[symbol]);  // Path compression
         }
         return parent[symbol];
     }
 
     // Function to union two sets
-    void unionSets(int symbol1, int symbol2)
-    {
+    void unionSets(int symbol1, int symbol2) {
         int root1 = find(symbol1);
         int root2 = find(symbol2);
 
-        if (root1 != root2)
-        {
+        if (root1 != root2) {
             parent[root1] = root2;
         }
     }
 
     // Getter for the parent map
-    const std::unordered_map<int, int> &getParentMap() const
-    {
+    const std::unordered_map<int, int>& getParentMap() const {
         return parent;
     }
 };
 
-class DominoChainFinder
-{
+class DominoChainFinder {
 private:
     UnionFind unionFind;
 
 public:
     // Function to add a domino to the chain
-    void addDomino(int symbol1, int symbol2)
-    {
+    void addDomino(int symbol1, int symbol2) {
         unionFind.makeSet(symbol1);
         unionFind.makeSet(symbol2);
         unionFind.unionSets(symbol1, symbol2);
     }
 
     // Function to find a chain starting from a given domino
-    std::vector<int> findChain(int startSymbol)
-    {
+    std::vector<int> findChain(int startSymbol) {
         std::vector<int> chain;
         int root = unionFind.find(startSymbol);
 
-        for (const auto &entry : unionFind.getParentMap())
-        {
-            if (unionFind.find(entry.first) == root)
-            {
+        for (const auto& entry : unionFind.getParentMap()) {
+            if (unionFind.find(entry.first) == root) {
                 chain.push_back(entry.first);
             }
         }
@@ -78,8 +65,7 @@ public:
     }
 };
 
-int main()
-{
+int main() {
     // Example usage
     DominoChainFinder dominoChainFinder;
 
@@ -94,8 +80,7 @@ int main()
 
     // Print the chain
     std::cout << "Domino Chain: ";
-    for (int symbol : chain)
-    {
+    for (int symbol : chain) {
         std::cout << symbol << " ";
     }
 
