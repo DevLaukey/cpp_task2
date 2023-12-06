29c2c77684e94b2476a9f4f1b5646c23d5ac9272
DevLaukey <devlaukeymwaura@gmail.com>
2023-12-06 09:27:49 +0300

    Added task1 domino

--- DominoChainFinder.cpp
+++ DominoChainFinder.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
-#include <chrono>
 
 class DominoChainFinder
 {
@@ -10,12 +9,14 @@ private:
     std::unordered_map<int, std::vector<int>> adjacencyList;
 
 public:
+    // Function to add a domino to the chain
     void addDomino(int symbol1, int symbol2)
     {
         adjacencyList[symbol1].push_back(symbol2);
         adjacencyList[symbol2].push_back(symbol1);
     }
 
+    // Function to find a chain starting from a given domino
     std::vector<int> findChain(int startSymbol)
     {
         std::vector<int> chain;
@@ -27,6 +28,7 @@ public:
     }
 
 private:
+    // Depth-First Search to find a chain
     void dfs(int currentSymbol, std::vector<int> &chain, std::unordered_map<int, bool> &visited)
     {
         visited[currentSymbol] = true;
@@ -42,36 +44,26 @@ private:
     }
 };
 
-// Function to measure the time usage for finding a chain
-void measureFindChainTime(const std::vector<int> &dataset)
-{
-    DominoChainFinder dominoChainFinder;
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    for (size_t i = 0; i < dataset.size(); i += 2)
-    {
-        dominoChainFinder.addDomino(dataset[i], dataset[i + 1]);
-    }
-
-    // Choose a specific domino as the starting point
-    std::vector<int> chain = dominoChainFinder.findChain(dataset[0]);
-
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-    std::cout << "Dataset Size: " << dataset.size() / 2 << ", Time: " << duration.count() << " microseconds\n";
-}
-
 int main()
 {
-    // Example dataset sizes
-    std::vector<int> dataset1 = {1, 2, 2, 3, 3, 4, 4, 5};
-    std::vector<int> dataset2 = {1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9};
+    // Example usage
+    DominoChainFinder dominoChainFinder;
 
-    // Measure the time for finding a chain with different dataset sizes
-    measureFindChainTime(dataset1);
-    measureFindChainTime(dataset2);
+    // Add dominoes to the chain
+    dominoChainFinder.addDomino(1, 2);
+    dominoChainFinder.addDomino(2, 3);
+    dominoChainFinder.addDomino(3, 4);
+    dominoChainFinder.addDomino(4, 5);
+
+    // Find a chain starting from a specific domino
+    std::vector<int> chain = dominoChainFinder.findChain(1);
+
+    // Print the chain
+    std::cout << "Domino Chain: ";
+    for (int symbol : chain)
+    {
+        std::cout << symbol << " ";
+    }
 
     return 0;
 }
